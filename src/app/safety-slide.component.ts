b6a6dc3dec38e36d1cb44510b2055dce7cf5c10c
chukui <chukui0202@gmail.com>
2016-10-07 21:53:34 +0800

    10-07

--- src/app/safety-slide.component.ts
+++ src/app/safety-slide.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'safety-slide',
   templateUrl: './safety-slide.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./safety-slide.component.css']
 })
 export class SafetySlideComponent {
 
